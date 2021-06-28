63b52a31b536500d065ab409abadcdcda7a5c2ba
sapire <sapir8886@gmail.com>
2021-06-28 14:19:26 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+char* taskGetName()
+//returns a unique ascii name of the thread, less than 19 bytes long
+{
+	//TODO
+}
+
+int taskGetId()
+{
+	//returns a unique id
+}
+
+bool taskShouldSuspend()
+{
+	//returns true if the task should be suspended
+}
+
+void taskSuspend()
+{
+	//suspends a task until released
+}
+
+void taskWait(int numOfSeconds)
+{
+	//suspends a task for n seconds
+}
+
+char* taskGetMem()
+{
+	//get the buffer
+}
+
+void taskReleaseMem()
+{
+	//release the buffer
+}
+
+int taskPrio()
+{
+	//return the priority of the task
+}
+
+void taskWake(int pid)
+{
+	//wake a task by its pid
+}
